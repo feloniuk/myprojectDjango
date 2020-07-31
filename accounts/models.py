7e501f28878d4cccdf8ea028a5719f2e591c370f
Feloniuk <s-feloniuk@ukr.net>
2020-07-31 20:52:50 +0300

    up-date #3.4 init models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Profile(models.Model):
+    id = models.AutoField(primary_key=True)
+    login = models.CharField(max_length=25, unique=True)
+    password = models.CharField(max_length=66)
+    nickname = models.CharField(max_length=25, unique=True, default=login)
+
+    def __str__(self):
+        return f'nickname - {self.nickname}, id - {self.id}'
